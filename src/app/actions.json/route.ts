ba55fd16b1594523b65fa939e6af6078c1b73722
Dharmin Nagar <dharminnagar@gmail.com>
2025-03-26 09:43:55 +0530

    feat: donate SOL Blink

---
+++ src/app/actions.json/route.ts
@@ -0,0 +1,18 @@
+import { ACTIONS_CORS_HEADERS, ActionsJson } from "@solana/actions";
+
+export const GET = async () => {
+    const payload: ActionsJson = {
+        rules: [
+            {
+                pathPattern: "/",
+                apiPath: "/api/action/donate",
+            },
+        ],
+    };
+
+    return new Response(JSON.stringify(payload), {
+        headers: ACTIONS_CORS_HEADERS,
+    });
+};
+
+export const OPTIONS = GET;
