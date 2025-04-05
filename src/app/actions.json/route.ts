b4f86b32be39644b25f6abc7e7b7e34fbf60471e
Dharmin Nagar <dharminnagar@gmail.com>
2025-04-05 13:47:21 +0530

    added

--- src/app/actions.json/route.ts
+++ src/app/actions.json/route.ts
@@ -1,18 +1,22 @@
 import { ACTIONS_CORS_HEADERS, ActionsJson } from "@solana/actions";
 
 export const GET = async () => {
-    const payload: ActionsJson = {
-        rules: [
-            {
-                pathPattern: "/",
-                apiPath: "/api/action/donate",
-            },
-        ],
-    };
+  const payload: ActionsJson = {
+    rules: [
+      {
+        pathPattern: "/",
+        apiPath: "/api/action/donate",
+      },
+      {
+        pathPattern: "/",
+        apiPath: "/api/action/donate-spl",
+      },
+    ],
+  };
 
-    return new Response(JSON.stringify(payload), {
-        headers: ACTIONS_CORS_HEADERS,
-    });
+  return new Response(JSON.stringify(payload), {
+    headers: ACTIONS_CORS_HEADERS,
+  });
 };
 
 export const OPTIONS = GET;
